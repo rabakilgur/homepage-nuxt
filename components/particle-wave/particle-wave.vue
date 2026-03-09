82ea3dca992f613a9b0e4175b2ac111c59649995
Robin Garbe <mail@robin-garbe.de>
2026-03-10 00:56:59 +0100

    design: add fade in animation to particle wave canvas

--- components/particle-wave/particle-wave.vue
+++ components/particle-wave/particle-wave.vue
@@ -9,5 +9,20 @@ onMounted(() => {
 </script>
 
 <template>
-  <canvas v-once ref="canvas"></canvas>
+  <canvas v-once ref="canvas" class="particlewave"></canvas>
 </template>
+
+<style scoped>
+.particlewave {
+  animation: fade-in 1s ease-in-out;
+}
+
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
